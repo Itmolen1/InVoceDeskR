85bedc3bc0b14cfd7399fc5549617725f6c01c1e
Samar Gul <samaritmolen@gmail.com>
2018-11-02 11:08:02 +0500

    samar

--- InvoiceDiskLast/Controllers/PurchaseController.cs
+++ InvoiceDiskLast/Controllers/PurchaseController.cs
@@ -1331,6 +1331,98 @@ namespace InvoiceDiskLast.Controllers
             public string Name { get; set; }
 
         }
+
+
+        public ActionResult PurchaseList()
+        {
+            return View();
+
+        }
+
+
+
+        [HttpPost]
+        public JsonResult GetPurderOrderList()
+        {
+            IEnumerable<MvcPurchaseModel> PurchaseList;
+            try
+            {
+                var draw = Request.Form.GetValues("draw").FirstOrDefault();
+                var start = Request.Form.GetValues("start").FirstOrDefault();
+                var length = Request.Form.GetValues("length").FirstOrDefault();
+                var sortColumn = Request.Form.GetValues("columns[" +
+                Request.Form.GetValues("order[0][column]").FirstOrDefault() + "][name]").FirstOrDefault();
+                var sortColumnDir = Request.Form.GetValues("order[0][dir]").FirstOrDefault();
+                string search = Request.Form.GetValues("search[value]")[0];
+
+                int pageSize = length != null ? Convert.ToInt32(length) : 0;
+                int skip = start != null ? Convert.ToInt32(start) : 0;
+
+                GlobalVeriables.WebApiClient.DefaultRequestHeaders.Clear();
+                int CompanyId = Convert.ToInt32(Session["CompayID"]);
+                GlobalVeriables.WebApiClient.DefaultRequestHeaders.Add("CompayID", CompanyId.ToString());
+
+                HttpResponseMessage response = GlobalVeriables.WebApiClient.GetAsync("APIPurchase").Result;
+                PurchaseList = response.Content.ReadAsAsync<IEnumerable<MvcPurchaseModel>>().Result;
+
+
+                if (!string.IsNullOrEmpty(search) && !string.IsNullOrWhiteSpace(search))
+                {
+                    PurchaseList = PurchaseList.Where(p => p.PurchaseOrderID.ToString().Contains(search)
+                  || p.PurchaseRefNumber != null && p.PurchaseRefNumber.ToLower().Contains(search.ToLower())
+                  || p.PurchaseDate != null && p.PurchaseDate.ToString().ToLower().Contains(search.ToLower())
+                  || p.PurchaseDueDate != null && p.PurchaseDueDate.ToString().ToLower().Contains(search.ToLower())
+                  || p.Status != null && p.Status.ToString().ToLower().Contains(search.ToLower())
+                  || p.PurchaseTotoalAmount != null && p.PurchaseTotoalAmount.ToString().ToLower().Contains(search.ToLower())
+                  || p.Status != null && p.Status.ToString().ToLower().Contains(search.ToLower())).ToList();
+                }
+
+
+                //    switch (sortColumn)
+                //{
+                //    case "ContactName":
+                //        ContactsList = ContactsList.OrderBy(c => c.ContactName);
+                //        break;
+                //    case "Type":
+                //        ContactsList = ContactsList.OrderBy(c => c.Type);
+                //        break;
+
+
+                //    case "BillingPersonName":
+                //        ContactsList = ContactsList.OrderBy(c => c.BillingPersonName);
+                //        break;
+
+                //    case "BillingCompanyName":
+                //        ContactsList = ContactsList.OrderBy(c => c.BillingCompanyName);
+                //        break;
+
+                //    case "BillingVatTRN":
+
+                //        ContactsList = ContactsList.OrderBy(c => c.BillingVatTRN);
+                //        break;
+
+                //    default:
+                //        ContactsList = ContactsList.OrderByDescending(c => c.ContactsId);
+                //        break;
+                //}
+
+
+                int recordsTotal = recordsTotal = PurchaseList.Count();
+                var data = PurchaseList.Skip(skip).Take(pageSize).ToList();
+                return Json(new { draw = draw, recordsFiltered = recordsTotal, recordsTotal = recordsTotal, data = data }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex.ToString());
+                Json(new { draw = 0, recordsFiltered = 0, recordsTotal = 0, data = 0 }, JsonRequestBehavior.AllowGet);
+            }
+            return Json(null, JsonRequestBehavior.AllowGet);
+
+        }
+
+
+
+
     }
 
 }
