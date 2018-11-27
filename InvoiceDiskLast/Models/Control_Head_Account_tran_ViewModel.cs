a685ecd670edeb2763b48517fcbf9f1f14d83ded
Rukhsar Ahmad <itmolen1@gmail.com>
2018-11-27 15:08:02 +0500

    Rukhsar Changes 307 11/27

---
+++ InvoiceDiskLast/Models/Control_Head_Account_tran_ViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InvoiceDiskLast.Models
+{
+    public class Control_Head_Account_tran_ViewModel
+    {
+        public int? ControlAccountId { get; set; }
+        public string ControleAccountTitile { get; set; }
+        public int? HeadAccountId { get; set; }
+        public string HeadAccountTitle { get; set; }
+        public int? AccountId { get; set; }
+        public string AccountTitle { get; set; }
+        public int Id { get; set; }
+        public double? AmountCredit { get; set; }
+        public double? AmountDebit { get; set; }
+    }
+}
