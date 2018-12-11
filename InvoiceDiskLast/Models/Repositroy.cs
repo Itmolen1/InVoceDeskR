a094da84ebf760e0c0d2419b154c33d9c484b376
Rukhsar Ahmad <itmolen1@gmail.com>
2018-12-11 10:08:04 +0500

    Rukhsar Changes 1007 12/11

---
+++ InvoiceDiskLast/Models/Repositroy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace InvoiceDiskLast.Models
+{
+    public class Repositroy<T>
+    {
+        private DBEntities Entities = new DBEntities();
+
+        //protected DbSet<T> DbSet
+        //{
+        //    get; set;
+        //}
+
+        public Repositroy()
+        {
+           // DbSet = Entities
+        }
+
+    }
+}
