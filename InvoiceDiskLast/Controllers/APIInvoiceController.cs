d90e36c87e8dc41e6c97761c397969761c45a25c
Rukhsar Ahmad <itmolen1@gmail.com>
2019-01-01 17:37:25 +0500

    Rukhsar changes 537 1/1 19

--- InvoiceDiskLast/Controllers/APIInvoiceController.cs
+++ InvoiceDiskLast/Controllers/APIInvoiceController.cs
@@ -11,6 +11,51 @@ namespace InvoiceDiskLast.Controllers
 {
     public class APIInvoiceController : ApiController
     {
+        private DBEntities db = new DBEntities();
 
+        [Route("api/GetInvoiceTable/{CompayID:int}")]
+        public IHttpActionResult GetInvoiceTables(int CompayID)
+        {
+            List<InvoiceViewModel> invoiceViewModel = new List<InvoiceViewModel>();
+
+
+            invoiceViewModel = db.InvoiceTables.Where(x => x.CompanyId == CompayID).Select(c => new InvoiceViewModel
+            {
+
+                InvoiceID = c.InvoiceID,
+                Invoice_ID = c.Invoice_ID,
+                RefNumber = c.RefNumber,
+                InvoiceDate = c.InvoiceDate,
+                InvoiceDueDate = c.InvoiceDueDate,
+                Vat = c.TotalVat6 + c.TotalVat21,
+                TotalAmount = c.TotalAmount,
+                Status = (c.Status).Trim(),
+                UserName = (c.UserTable.UserFname + " " + c.UserTable.UserLname != "" ? c.UserTable.UserFname + " " + c.UserTable.UserLname : c.UserTable.Username),
+                CustomerName = c.ContactsTable.ContactName
+
+            }).ToList();
+
+            return Ok(invoiceViewModel);
+
+
+        }
+
+        [Route("api/GetInvoiceCount")]
+        public IHttpActionResult GetInvoiceCount()
+        {
+            MVCInvoiceModel Invoice = new MVCInvoiceModel();
+
+            int InvoiceId = db.InvoiceTables.ToList().Count() + 1;
+            Invoice.Invoice_ID = InvoiceId.ToString();
+
+            if (InvoiceId == 0)
+            {
+                return Ok(Invoice);
+            }
+            else
+            {
+                return Ok(Invoice);
+            }
+        }
     }
 }
---
+++ InvoiceDiskLast/Models/InvoiceViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InvoiceDiskLast.Models
+{
+    public class InvoiceViewModel
+    {
+        public int InvoiceID { get; set; }
+        public string Invoice_ID { get; set; }
+        public string RefNumber { get; set; }
+        public Nullable<int> QutationId { get; set; }
+        public Nullable<System.DateTime> InvoiceDate { get; set; }
+        public Nullable<System.DateTime> InvoiceDueDate { get; set; }
+        public Nullable<double> SubTotal { get; set; }
+        public Nullable<double> TotalVat6 { get; set; }
+        public Nullable<double> TotalVat21 { get; set; }
+        public Nullable<double> DiscountAmount { get; set; }
+        public Nullable<double> TotalAmount { get; set; }
+        public string CustomerNote { get; set; }
+        public string Status { get; set; }
+        public Nullable<int> UserId { get; set; }
+        public Nullable<int> CompanyId { get; set; }
+        public Nullable<int> ContactId { get; set; }
+
+        public string UserName { get; set; }
+        public string CustomerName { get; set; }
+
+
+
+
+        public int InvoiceDetailId { get; set; }
+        public Nullable<int> ItemId { get; set; }
+        public string Description { get; set; }
+        public Nullable<int> Quantity { get; set; }
+        public Nullable<double> Rate { get; set; }
+        public Nullable<double> Total { get; set; }
+        public Nullable<double> Vat { get; set; }
+        public Nullable<double> RowSubTotal { get; set; }       
+        public string Type { get; set; }
+        public Nullable<System.DateTime> ServiceDate { get; set; }
+
+        public List<InvoiceDetailsTable> InvoiceDetailsTable { get; set; }
+
+    }
+}
