bef681b11ae719155c54eed6afd1f7d0f44718fa
Rukhsar Ahmad <itmolen1@gmail.com>
2019-01-05 14:36:49 +0500

    Rukhsar changes

---
+++ InvoiceDiskLast/Models/InvoiceModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InvoiceDiskLast.Models
+{
+    public class InvoiceModel
+    {
+        public int InvoiceID { get; set; }
+        public string Invoice_ID { get; set; }
+        public string RefNumber { get; set; }
+        public Nullable<int> QutationId { get; set; }
+        public Nullable<System.DateTime> InvoiceDate { get; set; }
+        public Nullable<System.DateTime> InvoiceDueDate { get; set; }
+        public Nullable<double> SubTotal { get; set; }
+        public Nullable<double> TotalVat6 { get; set; }
+        public Nullable<double> TotalVat21 { get; set; }
+        public Nullable<double> DiscountAmount { get; set; }
+        public Nullable<double> TotalAmount { get; set; }
+        public string CustomerNote { get; set; }
+        public string Status { get; set; }
+        public Nullable<int> UserId { get; set; }
+        public Nullable<int> CompanyId { get; set; }
+        public Nullable<int> ContactId { get; set; }
+        public string Type { get; set; }
+        public string InvoiceDescription { get; set; }
+    }
+}
