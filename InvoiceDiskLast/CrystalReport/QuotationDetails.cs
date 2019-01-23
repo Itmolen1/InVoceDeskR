f858be96803e4e1be12c0cd1e76d9b56c93de0d5
Rukhsar Ahmad <itmolen1@gmail.com>
2019-01-23 14:53:32 +0500

    Rukhsar changes

--- InvoiceDiskLast/CrystalReport/QuotationDetails.cs
+++ InvoiceDiskLast/CrystalReport/QuotationDetails.cs
@@ -14,9 +14,8 @@ namespace InvoiceDiskLast.CrystalReport {
     using CrystalDecisions.Shared;
     using CrystalDecisions.ReportSource;
     using CrystalDecisions.CrystalReports.Engine;
-
-  
-
+    
+    
     public class QuotationDetails : ReportClass {
         
         public QuotationDetails() {
